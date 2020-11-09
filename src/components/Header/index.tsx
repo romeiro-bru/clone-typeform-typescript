c95575e0aab8e0d1b926aef0cfcfbfb87ef5df4d
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-11-08 23:35:45 -0300

    Add FirstAndSecond, Add Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container } from './styles';
+import { useViewportScroll, useTransform } from 'framer-motion';
+
+const  Header: React.FC = () => {
+    const { scrollYProgress } = useViewportScroll()
+
+    const headerY = useTransform(scrollYProgress, [0.188, 0.198], ['0%', '-100%']);
+
+    return (
+        <Container style={{ y: headerY }} />
+       
+    )
+}
+
+export default Header;
